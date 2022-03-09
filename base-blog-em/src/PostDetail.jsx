56f08ddd25fcac09dddf2eaf2367d60da1a199f0
stephenavocado <stephenwalter@chicagobooth.edu>
2022-03-09 01:01:56 +0000

    fetch comments

--- base-blog-em/src/PostDetail.jsx
+++ base-blog-em/src/PostDetail.jsx
@@ -1,3 +1,5 @@
+import { useQuery } from "react-query";
+
 async function fetchComments(postId) {
   const response = await fetch(
     `https://jsonplaceholder.typicode.com/comments?postId=${postId}`
@@ -23,7 +25,15 @@ async function updatePost(postId) {
 
 export function PostDetail({ post }) {
   // replace with useQuery
-  const data = [];
+  const { data, isError, isLoading, error } = useQuery(
+    ["comments", post.id], 
+    () => fetchComments(post.id), 
+    {staleTime: 2000}
+  );
+  //known keys only refetch upon a trigger
+  //['comments', post,id] dependency array
+  if (isLoading) return <h3>Loading...</h3>;
+  if (isError) return <h3>{error.toString()}</h3>;
 
   return (
     <>
