347cf559eba20ea655d5ca6a8e2eb90d5efa32f9
stephenavocado <stephenwalter@chicagobooth.edu>
2022-03-09 00:36:40 +0000

    useQuery install

--- base-blog-em/src/App.jsx
+++ base-blog-em/src/App.jsx
@@ -1,13 +1,20 @@
-import { Posts } from "./Posts";
 import "./App.css";
+
+import { Posts } from "./Posts";
+
+import { QueryClient, QueryProvider } from "react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     // provide React Query client to App
-    <div className="App">
-      <h1>Blog Posts</h1>
-      <Posts />
-    </div>
+    <QueryClientProvider>
+      <div className="App">
+        <h1>Blog Posts</h1>
+        <Posts />
+      </div>
+		</QueryClientProvider>
   );
 }
 
